1067d01807998dd81a80396340d771525fb52b2b
pocky-git <997859540@qq.com>
2021-03-03 21:15:11 +0800

    webpack

---
+++ webpack/lesson14-生产环境基本配置/webpack.config.js
@@ -0,0 +1,109 @@
+const { resolve } = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+process.env.NODE_ENV = 'production'
+
+const commonCssLoader = [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    {
+        loader: 'postcss-loader',
+        options: {
+            postcssOptions: {
+                plugins: ['postcss-preset-env']
+            }
+        }
+    }
+]
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/build.js',
+        path: resolve(__dirname,'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [...commonCssLoader]
+            },
+            {
+                test: /\.less$/,
+                use: [...commonCssLoader, 'less-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                options: {
+                    fix: true
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                useBuiltIns: 'usage',
+                                corejs: {
+                                    version: 3
+                                },
+                                targets: {
+                                    chrome: '60',
+                                    firefox: '60',
+                                    ie: '9',
+                                    safari: '10',
+                                    edge: '17'
+                                }
+                            }
+                        ]
+                    ]
+                }
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 3 * 1024,
+                    name: '[hash:10].[ext]',
+                    esModule: false,
+                    outputPath: 'images'
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                exclude: /\.(html|js|css|less|jpg|png|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[hash:10].[ext]',
+                    outputPath: 'media'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/build.css'
+        }),
+        new OptimizeCssAssetsWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        })
+    ],
+    mode: 'production'
+}
